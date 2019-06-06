dd8faef1882025dd42ae68ac52f54526903bcaab
qiaokai <1941312688@qq.com>
2019-06-06 08:38:34 +0800

    测试提交

--- src/main/java/com/sinosoft/Application.java
+++ src/main/java/com/sinosoft/Application.java
@@ -2,8 +2,15 @@ package com.sinosoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+//扫描 mybatis mapper 包路径
+@MapperScan(basePackages = "com.sinosoft.mapper")
+//扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
+@ComponentScan(basePackages= {"com.sinosoft", "org.n3r.idworker"})
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/sinosoft/controller/UserContoller.java
+++ src/main/java/com/sinosoft/controller/UserContoller.java
@@ -1,18 +1,26 @@
 package com.sinosoft.controller;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sinosoft.bean.User;
+import com.sinosoft.dao.UserDao;
+import com.sinosoft.pojo.DemoJSONResult;
+import com.sinosoft.pojo.User;
 
 //@Controller
 @RestController //@RestController = @Controller + @ResponseBody
 @RequestMapping("user")
 public class UserContoller {
 
+	@Autowired
+	private UserDao uUserDao ;
+	
 	@RequestMapping("/getUser")
 //	@ResponseBody
 	public User getUser() {
@@ -22,9 +30,45 @@ public class UserContoller {
 		u.setAge(18);
 		u.setBirthday(new Date());
 		u.setPassword("qiaokai");
-		u.setDesc(null);
+		u.setuDesc(null);
 		
 		return u;
 	}
 
+	@RequestMapping("/saveUser")
+	public DemoJSONResult saveUser()
+	{
+       SimpleDateFormat sFormat = new SimpleDateFormat("YYYY-MM-DD");
+		
+		
+		User uUser = new User() ;
+		uUser.setUserid("0099887653222");
+		uUser.setName("乔开");
+		uUser.setAge(28);
+		uUser.setSex(0);
+		try
+		{
+			uUser.setBirthday(sFormat.parse("1992-11-28"));
+		}
+		catch (ParseException e)
+		{
+			e.printStackTrace();
+		}
+		uUser.setIdcrad("610126199211285653");
+		uUser.setPassword("11111111");
+		uUser.setMakedate(new Date());
+		uUser.setMaketime(new Date());
+		
+		try
+		{
+			uUserDao.saveUser(uUser);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return DemoJSONResult.ok("保存成功");
+	}
+	
+	
 }
